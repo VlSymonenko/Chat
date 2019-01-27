7aec0b9f85b8856f20946349e0db98342831b5ad
VlSymonenko <symonenkovlad13@gmail.com>
2019-01-27 23:15:57 +0200

    add node modules

---
+++ backend/App.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+
+let server = app.listen(8080, function(){
+    console.log('server is running on port 8080')
+});
+
+let io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
